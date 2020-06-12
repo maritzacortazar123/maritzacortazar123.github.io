053587166a71ae0b60ec7a7bd4ec63411559fdb0
Maritza Cortazar <maritzacortazar@hotmail.com>
2020-06-11 20:39:13 -0500

    Agregar todos los archivos

---
+++ scripts/menu.js
@@ -0,0 +1,6 @@
+var icmenu = document.getElementById('icmenu');
+var btnmenu = document.getElementById('btnmenu');
+icmenu.addEventListener('click' function(){
+	'use strict';
+	btnmenu.classList.toggle('mostrar');
+});
